28539ef9914a7c1cdb050a80128570cd60ad8856
hb0730 <1278032416@qq.com>
2022-08-08 10:58:25 +0800

    chore: update cli version

--- go.mod
+++ go.mod
@@ -1,16 +1,15 @@
 module drone-plugin-gitea-release-change-log
 
-go 1.16
+go 1.18
 
 require (
 	code.gitea.io/sdk/gitea v0.15.1
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gookit/gitw v0.2.5
 	github.com/gookit/goutil v0.5.7
 	github.com/hashicorp/go-version v1.3.0 // indirect
 	github.com/joho/godotenv v1.4.0
 	github.com/kr/pretty v0.2.0 // indirect
-	github.com/urfave/cli v1.22.9
+	github.com/urfave/cli/v2 v2.11.1
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 )
@@ -10,97 +10,98 @@ import (
 var version = "unknown"
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "drone-plugin-gitea-release-change-log"
-	app.Description = "update/create gitea release change log drone plugin"
-	app.Copyright = "© 2021-now hb0730"
-	app.Action = run
-	app.Version = version
-	app.Authors = []cli.Author{
-		{
-			Name:  "hb0730",
-			Email: "huangbing0730@gmail.com",
+	app := &cli.App{
+		Name:      "drone-plugin-gitea-release-change-log",
+		Usage:     "update/create gitea release change log drone plugin",
+		Copyright: "© 2021-now hb0730",
+		Action:    run,
+		Version:   version,
+		Authors: []*cli.Author{
+			{
+				Name:  "hb0730",
+				Email: "huangbing0730@gmail.com",
+			},
 		},
-	}
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:   "debug",
-			Usage:  "debug",
-			EnvVar: "PLUGIN_DEBUG,DEBUG",
-		},
-		cli.StringFlag{
-			Name:   "drone.tag",
-			Usage:  "drone tag",
-			EnvVar: "DRONE_TAG",
-		},
-		cli.StringFlag{
-			Name:   "drone.repo",
-			Usage:  "drone repo",
-			EnvVar: "DRONE_REPO",
-		},
-		cli.StringFlag{
-			Name:   "drone.repo.name",
-			Usage:  "drone repo name",
-			EnvVar: "DRONE_REPO_NAME",
-		},
-		cli.StringFlag{
-			Name:   "drone.repo.owner",
-			Usage:  "drone repo owner",
-			EnvVar: "DRONE_REPO_OWNER",
-		},
-		cli.StringFlag{
-			Name:   "drone.commit",
-			Usage:  "drone commit hash",
-			EnvVar: "DRONE_COMMIT",
-		},
-		cli.StringFlag{
-			Name:   "gitea.url",
-			Usage:  "gite base url",
-			EnvVar: "PLUGIN_GITEA_URL,PLUGIN_URL,GITEA_URL,URL",
-		},
-		cli.StringFlag{
-			Name:   "gitea.token",
-			Usage:  "gitea user token",
-			EnvVar: "PLUGIN_GITEA_TOKEN,PLUGIN_TOKEN,GITEA_TOKEN,TOKEN",
-		},
-		cli.StringFlag{
-			Name:   "changelog.config",
-			Usage:  "the YAML config file for generate changelog",
-			EnvVar: "PLUGIN_CHANGE_LOG_CONFIG,PLUGIN_LOG_CONFIG,CHANGE_LOG_CONFIG,CONFIG",
-		},
-		cli.StringFlag{
-			Name:   "changelog.config_str",
-			Usage:  "the YAML string type for generate changelog,priority is lower than file config",
-			EnvVar: "PLUGIN_CHANGE_LOG_CONFIG_STR,PLUGIN_LOG_CONFIG_STR,CHANGE_LOG_CONFIG_STR,CONFIG_STR",
-		},
-		cli.IntFlag{
-			Name:   "changelog.tag_type",
-			Usage:  "repo tags sort type,default: 1 creatordate sort",
-			EnvVar: "PLUGIN_CHANGE_LOG_TAG_TYPE,PLUGIN_TAG_TYPE,CHANGE_LOG_TAG_TYPE,LOG_TAG_TYPE",
-			Value:  1,
-		},
-		cli.StringFlag{
-			Name:   "changelog.repo_path",
-			Usage:  "git repo path,default:./",
-			EnvVar: "PLUGIN_CHANGE_LOG_REPO_PATH,PLUGIN_REPO_PATH,CHANGE_LOG_REPO_PATH,REPO_PATH",
-			Value:  "./",
-		},
-		cli.StringFlag{
-			Name:   "changelog.sha1",
-			Usage:  "The old git sha version. allow: tag name, commit id",
-			EnvVar: "PLUGIN_CHANGE_LOG_SHA1,PLUGIN_SHA1,CHANGE_LOG_SHA1,SHA1",
-			Value:  "prev",
-		},
-		cli.StringFlag{
-			Name:   "changelog.sha2",
-			Usage:  "The new git sha version. allow: tag name, commit id",
-			EnvVar: "PLUGIN_CHANGE_LOG_SHA2,PLUGIN_SHA2,CHANGE_LOG_SHA2,SHA2",
-			Value:  "last",
-		},
-		cli.BoolFlag{
-			Name:   "changelog.verbose",
-			Usage:  "show more information",
-			EnvVar: "PLUGIN_CHANGE_LOG_VERBOSE,PLUGIN_LOG_VERBOSE,CHANGE_LOG_VERBOSE,VERBOSE",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "debug",
+				Usage:   "debug mode",
+				EnvVars: []string{"PLUGIN_DEBUG", "DEBUG"},
+			},
+			&cli.StringFlag{
+				Name:    "drone.tag",
+				Usage:   "drone tag",
+				EnvVars: []string{"DRONE_TAG"},
+			},
+			&cli.StringFlag{
+				Name:    "drone.repo",
+				Usage:   "drone repo",
+				EnvVars: []string{"DRONE_REPO"},
+			},
+			&cli.StringFlag{
+				Name:    "drone.repo.name",
+				Usage:   "drone repo name",
+				EnvVars: []string{"DRONE_REPO_NAME"},
+			},
+			&cli.StringFlag{
+				Name:    "drone.repo.owner",
+				Usage:   "drone repo owner",
+				EnvVars: []string{"DRONE_REPO_OWNER"},
+			},
+			&cli.StringFlag{
+				Name:    "drone.commit",
+				Usage:   "drone commit hash",
+				EnvVars: []string{"DRONE_COMMIT"},
+			},
+			&cli.StringFlag{
+				Name:    "gitea.url",
+				Usage:   "gite base url",
+				EnvVars: []string{"PLUGIN_GITEA_URL", "PLUGIN_URL", "GITEA_URL,URL"},
+			},
+			&cli.StringFlag{
+				Name:    "gitea.token",
+				Usage:   "gitea user token",
+				EnvVars: []string{"PLUGIN_GITEA_TOKEN", "PLUGIN_TOKEN", "GITEA_TOKEN", "TOKEN"},
+			},
+			&cli.StringFlag{
+				Name:    "changelog.config",
+				Usage:   "the YAML config file for generate changelog",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_CONFIG", "PLUGIN_LOG_CONFIG", "CHANGE_LOG_CONFIG", "CONFIG"},
+			},
+			&cli.StringFlag{
+				Name:    "changelog.config_str",
+				Usage:   "the YAML string type for generate changelog,priority is lower than file config",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_CONFIG_STR", "PLUGIN_LOG_CONFIG_STR", "CHANGE_LOG_CONFIG_STR", "CONFIG_STR"},
+			},
+			&cli.IntFlag{
+				Name:    "changelog.tag_type",
+				Usage:   "repo tags sort type,default: 1 creatordate sort",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_TAG_TYPE", "PLUGIN_TAG_TYPE", "CHANGE_LOG_TAG_TYPE", "LOG_TAG_TYPE"},
+				Value:   1,
+			},
+			&cli.StringFlag{
+				Name:    "changelog.repo_path",
+				Usage:   "git repo path,default:./",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_REPO_PATH", "PLUGIN_REPO_PATH", "CHANGE_LOG_REPO_PATH", "REPO_PATH"},
+				Value:   "./",
+			},
+			&cli.StringFlag{
+				Name:    "changelog.sha1",
+				Usage:   "The old git sha version. allow: tag name, commit id",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_SHA1", "PLUGIN_SHA1", "CHANGE_LOG_SHA1", "SHA1"},
+				Value:   "prev",
+			},
+			&cli.StringFlag{
+				Name:    "changelog.sha2",
+				Usage:   "The new git sha version. allow: tag name, commit id",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_SHA2,PLUGIN_SHA2", "CHANGE_LOG_SHA2", "SHA2"},
+				Value:   "last",
+			},
+			&cli.BoolFlag{
+				Name:    "changelog.verbose",
+				Usage:   "show more information",
+				EnvVars: []string{"PLUGIN_CHANGE_LOG_VERBOSE", "PLUGIN_LOG_VERBOSE", "CHANGE_LOG_VERBOSE", "VERBOSE"},
+			},
 		},
 	}
 	if _, err := os.Stat("/run/drone/env"); err == nil {
