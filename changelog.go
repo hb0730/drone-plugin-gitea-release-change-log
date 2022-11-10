5f87cafa371d9723350390b9e2bf1de915ce7fa0
hb0730 <1278032416@qq.com>
2022-11-10 09:05:30 +0800

    fix: drone.repo.branch is null

--- changelog.go
+++ changelog.go
@@ -51,7 +51,7 @@ func (l ChangeLog) PutRelease(config ChangeLogConfig) error {
 	if resp.StatusCode == 404 {
 		option := gitea.CreateReleaseOption{
 			TagName: l.CurrentTag,
-			Target:  l.Drone.RepoBrand,
+			Target:  config.Sha1,
 			Title:   l.CurrentTag,
 			Note:    changelog,
 		}
